2d55544b61a8160842303c1d88bfd62512000166
JeongTaeGo <giwigi@naver.com>
2024-01-08 17:32:06 +0900

    make list and push the List information

--- lib/subDetail.dart
+++ lib/subDetail.dart
@@ -1,27 +1,50 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-class SubDetail extends StatefulWidget{
+class SubDetail extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SubDetail();
 }
 
-class _SubDetail extends State<SubDetail>{
-  @override
-  Widget build(BuildContext context){
+class _SubDetail extends State<SubDetail> {
+  List<String> todoList = new List.empty(growable: true);
 
+  @override
+  void initState() {
+    super.initState();
+    todoList.add('당근 사오기');
+    todoList.add('약 사오기');
+    todoList.add('청소하기');
+    todoList.add('부모님께 전화하기');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sub Detail'),
       ),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: (){
-              Navigator.of(context).pushNamed('/second'); //스택 메모리에 있는 자료를 교체하는 코드
-            },
-            child: Text('두번째 페이지로 이동'),
-          ),
-        ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell(
+              child: Text(
+                todoList[index],
+                style: TextStyle(fontSize: 30),
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .pushNamed('/third', arguments: todoList[index]);
+              },
+            ),
+          );
+        },
+        itemCount: todoList.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
